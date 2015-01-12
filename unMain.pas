3d6917ca61f3e1bccf6a91d15d04c6465f9e4483
Alexey T <support@uvviewsoft.com>
2015-01-12 18:49:11 +0300

    rename "CSS"

--- unMain.pas
+++ unMain.pas
@@ -3407,7 +3407,7 @@ procedure MsgCannotCreate(const fn: Widestring; H: THandle);
 function SynAppdataDir: string;
 
 const
-  cSynVer = '6.15.1900';
+  cSynVer = '6.15.1902';
   cSynPyVer = '1.0.146';
 
 const
@@ -3543,7 +3543,7 @@ end;
 
 
 const
-  cLexerCss = 'Style sheets';
+  cLexerCss = 'CSS';
   cLexerCssList = 'LESS,SASS,SCSS,Sass,Stylus';
   cLexerText = 'Text files';
   cLexerIni = 'Ini files';
